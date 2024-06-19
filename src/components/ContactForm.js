d01fe383dae9007ee385f94bba6b67b21c33ec76
jrvscm <chrisjarvisdev@gmail.com>
2024-06-19 15:03:38 -0600

    update static form

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -39,7 +39,7 @@ export const ContactForm = () => {
     event.preventDefault();
     startLoading();
     try {
-      const response = await fetch('/', {
+      const response = await fetch('/__forms.html', {
           method: 'POST',
           headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
           body: new URLSearchParams(formData).toString(),
