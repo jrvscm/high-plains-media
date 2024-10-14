c42efee3d5ba2f1f62cbd40f37c9f3ef0fc2da1e
jrvscm <chrisjarvisdev@gmail.com>
2024-10-14 14:12:45 -0600

    more updates

--- src/pages/projects/[project].js
+++ src/pages/projects/[project].js
@@ -21,35 +21,94 @@ const Wrapper = styled.div`
 `;
 
 const Hero = styled(Container)`
-    background: url('images/julieschf-homepage.png');
-    background-size: contain;
-    background-position: center;
-    border: 2px solid ${({ theme }) => theme.colors.sectionBackground};
-    border-top-right-radius: 25px;
-    border-top-left-radius: 25px;
-    border-bottom-right-radius: 25px;
-    border-bottom-left-radius: 25px;
-    height: 80vh;
+    background: url('/images/julieschf-homepage.png') top center no-repeat;
+    background-size: cover;
+    ${'' /* border: 5px solid ${({ theme }) => theme.colors.black}; */}
+    border: 5px solid turquoise;
+    border-radius: 25px;
     width: 80%;
+    aspect-ratio: 5 / 3; /* Aspect ratio 4:3 for the Hero component */
     margin-top: 100px;
     position: relative;
 `;
 
 const Phone = styled.div`
-  height: 50vh;
+  background: url('/images/julieschf_reg_page.png') bottom center no-repeat;
+  background-size: cover;
+  aspect-ratio: 8 / 16; /* Aspect ratio 9:16 for the Phone component */
   width: 20%;
   z-index: 1;
-  border: 2px solid ${({ theme }) => theme.colors.sectionBackground};
-  border-top-right-radius: 25px;
-  border-top-left-radius: 25px;
-  border-bottom-right-radius: 25px;
-  border-bottom-left-radius: 25px;
-
+  ${'' /* border: 5px solid ${({ theme }) => theme.colors.black}; */}
+  border: 5px solid turquoise;
+  border-radius: 25px;
   position: absolute;
   bottom: -10%;
-  right: -5%;
+  right: -8%;
+  box-shadow: 0px 8px 16px rgba(0, 0, 0, 0.3); /* Add shadow here */
 `;
 
+const Technologies = styled.div`
+    background: ${({ theme }) => theme.colors.primaryLight};
+    height: 100px;
+    border-radius: 50px;
+    display: flex;
+    justify-content: space-evenly;
+    align-items: center;
+    box-shadow: inset 0px 4px 10px rgba(0, 0, 0, 0.6); /* Inset shadow for depth effect */
+    text-transform: uppercase; 
+    ${'' /* width: 35vw; */}
+    width: 50vw;
+    z-index:1;
+    margin: 140px auto 60px auto;
+    border: 5px solid turquoise;
+
+    color: ${({ theme }) => theme.colors.white};
+    font-family: 'Orbitron', sans-serif;
+    font-size: 32px;
+    ${({ $isMobile }) => $isMobile && css`
+        width: 100%;
+    `}
+`;
+
+const H3 = styled.h3`
+  margin: 0;
+  margin-right: 20px;
+  padding: 0;
+  letter-spacing: 2px;
+  text-shadow: 2px 2px 6px rgba(0, 0, 0, 0.5);
+`;
+
+const IconWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  gap: 20px; /* Space between icons */
+  padding: 0; 
+  margin: 0;
+
+  img {
+    height: 100%;
+    width: auto;
+  }
+
+  /* Add a vertical divider between each image */
+  img:not(:first-child) {
+    border-left: 2px solid ${({ theme }) => theme.colors.white}; /* Divider color */
+    padding-left: 20px; /* Space after divider */
+  }
+`;
+
+const DescriptionWrapper = styled(Container)`
+  font-family: 'Raleway', sans-serif;
+  font-size: 18px;
+  color: ${({ theme }) => theme.colors.white};
+`;
+
+const Description = styled.p`
+  margin: 0;
+  padding: 0;
+  text-align: center;
+`;
 
 const Project = () => {
   const router = useRouter();
@@ -71,12 +130,25 @@ const Project = () => {
       {SplashComponent}
       {!isMobile && <CustomCursor hovered={hovered}/>}
       <Wrapper>
-        {/* <Container>
+        <Container>
             <HeaderPill title={'work'} />
-        </Container> */}
+        </Container>
         <Hero>
-          <Phone></Phone>
+          <Phone />
         </Hero>
+        <Technologies>
+          <H3>BUILT WITH</H3>
+          <IconWrapper>
+            <img src="/images/tech-stack-nextjs.svg" />
+            <img src="/images/tech-stack-react.svg" />
+            <img src="/images/tech-stack-netlify.svg" />
+            <img src="/images/tech-stack-figma.svg" />
+          </IconWrapper>
+        </Technologies>
+        <DescriptionWrapper>
+          <Description>Julie&apos;s Caring Heart Foundation is a non profit in Gillette, Wyoming USA. They fund raise for charitable causes. 
+          Julie&apos;s Caring Heart Foundation's annual golf tournament needed a website that could handle player signups, sponsor signups, and let users make and manage payments.</Description>
+        </DescriptionWrapper>
       </Wrapper>
     </>
   );
